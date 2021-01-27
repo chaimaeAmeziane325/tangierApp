1648fd3f879c87add4b03490f532e4afe732fe3d
chaimaeAmeziane325 <77857287+chaimaeAmeziane325@users.noreply.github.com>
2021-01-27 22:15:31 +0100

    Add files via upload

---
+++ app/src/main/java/com/example/tangierapplication/Hotel.kt
@@ -0,0 +1,12 @@
+package com.example.tangierapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_hotel.*
+
+class Hotel : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hotel)
+    }
+}
